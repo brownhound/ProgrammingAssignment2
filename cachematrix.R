b555ff715e148e4ca3527870847096bd4f9c66b5
Ben <bggaston@gmail.com>
2017-10-29 16:23:23 -0400

    Added code per assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions will take what is assumed to be an invertible matrix (no
+## validation on input format), check if the inverse exists.  If the inverse
+## is already stored, it will return the stored value.  If it is not stored, 
+## it will calculate and return the inverse of the matrix.
 
-## Write a short comment describing this function
 
-##another short edit
+## Takes an invertible matrix, nulls the invert variable.  Can set, get, set 
+## the inverse of the matrix, or get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    invert <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    setinverse <- function(inverse) invert <<- inverse
+    getinverse <- function() invert
+    list(set = set, get = get, setinverse = setinverse, 
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Checks if invert is not null, and reports value if so.  If null, calculates
+## the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invert <- x$getinverse()
+    if(!is.null(invert)){
+        message("getting inverse matrix")
+        return(invert)
+    }
+    data <- x$get()
+    invert <- solve(data, ...)
+    x$setinverse(invert)
+    invert
 }
